5c64456126d5712b4eb7b3be6323a04438e3b28f
antekirta <murashkin.kirill.konst@gmail.com>
2020-11-07 11:28:11 +0200

    Create menu

--- src/main.c
+++ src/main.c
@@ -9,34 +9,10 @@ struct Product {
     * next;
 };
 
-struct List_Pointer {
-    char name[50];
-    Product **pointer_to_list;
-};
-
-List_Pointer **lists_of_lists;
+struct Product *list_of_products;
 
 void create_list() {
-    struct List_Pointer list_item;
-    Product *list[sizeof(Product)];
-    int number_of_lists = sizeof(lists_of_lists) / sizeof(List_Pointer);
 
-    // Allocate memory for one more item
-    lists_of_lists = (List_Pointer **) malloc((number_of_lists + 1) * sizeof(List_Pointer));
-
-    // Allocate memory for one product
-    // list = (Product *) malloc(sizeof(Product));
-
-    list[0] = NULL;
-
-    strcpy_s(list_item.name, "New list item!");
-    list_item.pointer_to_list = &list[0];
-
-    lists_of_lists[number_of_lists] = list_item;
-
-    system("cls");
-
-    printf("New list of products has been created! \n");
 };
 
 void show_list();
@@ -53,10 +29,24 @@ void load_list_from_file();
 
 void quit();
 
-int main(int argc, char const *argv[]) {
-//    print_menu();
+int main() {
+    print_menu();
 
-    create_list();
+    int menu_choice;
+
+    scanf("%d", &menu_choice);
+
+    switch (menu_choice)
+    {
+        case 1: printf("create_list"); break;
+        case 2: printf("show_list"); break;
+        case 3: printf("add_record"); break;
+        case 4: printf("update_record"); break;
+        case 5: printf("delete_record"); break;
+        case 6: printf("save_list_to_file"); break;
+        case 7: printf("load_list_from_file"); break;
+        case 8: printf("quit"); break;
+    }
 
     system("pause");
 
